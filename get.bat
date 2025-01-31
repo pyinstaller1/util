cc1cf310c70530e7b904414832322695955f1646
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2025-02-01 00:28:44 +0900

    Add files via upload

---
+++ get.bat
@@ -0,0 +1,27 @@
+rmdir /s /q util
+git clone https://github.com/pyinstaller1/util.git
+xcopy C:\Users\%USERNAME%\util\* C:\Users\%USERNAME%\ /Y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:: github로 보내기
+:: git add %1.py
+:: git commit -m "Add %1.py"
+:: git push origin master
+
+:: git init
+:: git config --global user.email "your_email@example.com"
+:: git config --global user.name "Your Name"
