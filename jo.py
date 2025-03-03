6f1707d778697ef97895e8001272858a8fa56146
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2025-03-03 20:53:08 +0900

    Add files via upload

--- jo.py
+++ jo.py
@@ -675,8 +675,7 @@ def on():
     if desktop in ["DESKTOP-MA2NLC4"]:
         if gw.getWindowsWithTitle('Chosun2M'):
             os.system("taskkill /F /IM _gins321.exe")
-        os.system("taskkill /F /IM Chosun2M.exe")
-        time.sleep(5)        
+        time.sleep(10)        
         subprocess.Popen(r"D:\Program Files\Chosun2M\Chosun2M.exe", shell=True)  # 프로그램 실행
         time.sleep(15)
 
