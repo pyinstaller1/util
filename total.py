fa39e2518a89ff35b8fd418a8cace8fb6891190c
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2025-04-15 04:48:12 +0900

    Add files via upload

--- total.py
+++ total.py
@@ -20,21 +20,13 @@ import os
 # 달조 매일 오전 5시 dal.play_dal("던전0")
 # 달조 매일 오전 7시 dal.play_dal("던전1")
 
+# 아레스 매일 오전 5시 dal.play_dal("던전0")
+
 # 아스달 매일 오전 6시 세력 임무 시작 ar.dungeon_ar()
 # 아스달 매일 오전 8시 세력 임무 완료 ar.dungeon_ar_end()
 
 
 
-
-
-
-
-
-# 수요일 점검
-# 달조 오후 3시 dal.check_dal()
-
-
-# 목요일 점검
 
 
 
@@ -44,14 +36,15 @@ print("Total 시작   " + time.strftime("%H:%M", time.localtime()))
 
 while True:
     if time.localtime().tm_wday == 0 and time.localtime().tm_hour == 7 and time.localtime().tm_min == 30:   # 월요일
-        print("월요일 주간 던전")
-        print("주간 오전 7시 30분 작업")
-        ares.dungeon_week_ares()   # 아레스 주간 던전
+        print("월요일 주간 던전 오전 7시 30분 작업")
+
+        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-H9B70U0"]:   # 아레스 주간 던전
+            ares.dungeon_week_ares()   # 아레스 주간 던전
             
     if time.localtime().tm_wday == 1 and time.localtime().tm_hour == 7 and time.localtime().tm_min == 30:   # 화요일
-        print("화요일 주간 던전")
-        print("주간 오전 7시 30분 작업")
-        ares.dungeon_week_ares()   # 아레스 주간 던전
+        print("화요일 주간 던전 오전 7시 30분 작업")
+        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-H9B70U0"]:   # 아레스 주간 던전
+            ares.dungeon_week_ares()   # 아레스 주간 던전
 
             
     if time.localtime().tm_wday == 2 and time.localtime().tm_hour == 10 and time.localtime().tm_min == 57:   # 수요일
@@ -63,8 +56,6 @@ while True:
     # 매일 오전 5시 20분
     if time.localtime().tm_hour == 5 and time.localtime().tm_min == 20:
         print("오전 5시 작업")
-        time.sleep(np.random.randint(30, 180)) # 30초~8분30초 랜덤 대기
-
 
         if desktop in ["DESKTOP-OHGK5MV"]:
             try:
@@ -74,11 +65,11 @@ while True:
 
 
 
-        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB"]:   # 아레스 던전
+        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-H9B70U0"]:   # 아레스 던전
             try:
                 ares.dungeon_ares()
             except Exception as e:
-                print(f"ar.dungeon_ar() 오류")
+                print(f"ares.dungeon_ares() 오류")
 
                 
         # 달조 던전   5시  7시
@@ -88,10 +79,9 @@ while True:
     # 매일 오전 6시 20분
     if time.localtime().tm_hour == 6 and time.localtime().tm_min == 20:
         print("오전 6시 작업")
-        time.sleep(np.random.randint(30, 180)) # 30초~8분30초 랜덤 대기
 
         # 아스달 던전 6시  8시
-        if desktop in ["DESKTOP-OHGK5MV", "DESKTOP-MA2NLC4", "DESKTOP-792RKKB"]:    # 아스달 세력임무
+        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-H9B70U0"]:    # 아스달 세력임무
             try:
                 ar.dungeon_ar()
             except Exception as e:
@@ -162,30 +152,19 @@ while True:
 
 
 
-    # 매일 오전 9시 play
-    if time.localtime().tm_hour == 9 and time.localtime().tm_min == 20:
-        print("오전 9시 작업")
-        time.sleep(np.random.randint(30, 511)) # 30초~8분30초 랜덤 대기'
-
-        # 달조 던전   5시  7시
-        # 아스달 던전 6시  8시
-        # 아레스 던전 5시
-        
-
-
     # 매시간 1분마다 play    
     if time.localtime().tm_min == 1:
         print(time.strftime("%H:%M", time.localtime()))
         time.sleep(np.random.randint(30, 180)) # 30초~3분 랜덤 대기
 
 
-        if desktop in ["DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-LRGAL8H", "DESKTOP-NT06800"]:
+        if desktop in ["DESKTOP-LRGAL8H", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-NT06800"]:
             try:
                 jo.play_lo()
             except:
                 print("jo.play_jo() 오류")
 
-        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-LRGAL8H", "DESKTOP-H9B70U0"]:
+        if desktop in ["DESKTOP-LRGAL8H", "DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-H9B70U0"]:
             try:
                 lo.play_lo()
             except:
@@ -216,7 +195,7 @@ while True:
                 print("ak.play() 오류")
 
 
-        if desktop in ["DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-LRGAL8H", "DESKTOP-H9B70U0"]:
+        if desktop in ["DESKTOP-LRGAL8H", "DESKTOP-MA2NLC4", "DESKTOP-792RKKB", "DESKTOP-OHGK5MV", "DESKTOP-H9B70U0"]:
             try:
                 rf.play_lo()
             except:
@@ -228,10 +207,8 @@ while True:
 
 
 
-    time.sleep(10)
-    if keyboard.is_pressed('esc'):  # ESC 키가 눌렸는지 확인
-        print("esc")
-        break
+    time.sleep(30)
+
 
 print("Total 종료" + " " + time.strftime("%H:%M", time.localtime()))
 
