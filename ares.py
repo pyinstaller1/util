9ea057d92ba11bff2938f95cdda6d0404628bfdf
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2025-08-15 13:37:53 +0900

    Add files via upload

--- ares.py
+++ ares.py
@@ -1520,9 +1520,9 @@ def on():
     pyautogui.mouseUp()
 
     if os.environ.get('COMPUTERNAME') in ["DESKTOP-MA2NLC4"]:
-        pyautogui.write('s0700921@nate.com')
-    if os.environ.get('COMPUTERNAME') in ["DESKTOP-792RKKB"]:
         pyautogui.write('s070092@nate.com')
+    if os.environ.get('COMPUTERNAME') in ["DESKTOP-792RKKB"]:
+        pyautogui.write('s0700921@nate.com')
     if os.environ.get('COMPUTERNAME') in ["DESKTOP-OHGK5MV"]:
         pyautogui.write('s070092@kakao.com')
     if os.environ.get('COMPUTERNAME') in ["DESKTOP-H9B70U0"]:
